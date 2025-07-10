7b85d1df5f5b2424193569bde0d2b17c4f040a1d
LegionAndres <andressaultan360@gmail.com>
2025-07-10 18:36:59 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('cambio-unico-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/styles.css',
+        '/app.js'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
